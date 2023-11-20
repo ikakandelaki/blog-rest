c14475e61792d797afc491856fb3ed101a46fb39
ikakandelaki <ikakandelaki777@gmail.com>
2023-11-21 00:13:35 +0400

    update migration file

--- src/main/resources/db/migration/V2023.11.20.1__create_posts_table.sql
+++ src/main/resources/db/migration/V2023.11.20.1__create_posts_table.sql
@@ -1,8 +1,9 @@
 create table posts
 (
-    id          bigint primary key auto_increment,
+    id          bigint auto_increment,
     title       varchar(255) not null,
     description text         not null,
     content     text         not null,
-    unique UQ_Posts_Title (title)
+    constraint PK_Posts primary key (id),
+    constraint UQ_Posts_Title unique (title)
 );
